075eb085adb7631624c5c0382f27d2a320fb031d
taylorj8 <taylorj8@tcd.ie>
2025-03-17 21:57:59 +0000

    q-learning attempt 3

--- qlearner.py
+++ qlearner.py
@@ -12,67 +12,57 @@ class QLearner:
         self.episodes = episodes
         self.alpha = 0.5
         self.gamma = 1.0
-        self.epsilon = 0.1
+        self.epsilon = 0.5
 
+    def get_q_value(self, state, move):
+        if state not in self.q_table:
+            self.q_table[state] = {m: default_q for m in self.game.get_remaining_moves()}
+        if move not in self.q_table[state]:
+            self.q_table[state] = default_q
+        return self.q_table[state][move]
 
-    def choose_move(self, state: str, maxing: bool) -> int:
-        if state not in self.q_table or random.uniform(0, 1) < self.epsilon:
-            return random.choice(self.game.get_remaining_moves())
+    def choose_move(self, state: str) -> int:
+        remaining_moves = self.game.get_remaining_moves()
+        if random.uniform(0, 1) < self.epsilon:
+            return random.choice(remaining_moves)
+        q_values = [self.get_q_value(state, move) for move in remaining_moves]
 
-        moves = self.game.get_remaining_moves()
-        if maxing:
-            max_q = max(self.q_table[state][m] for m in moves)
-            best_moves = [m for m in moves if self.q_table[state][m] == max_q]
+        max_q = max(q_values)
+        if q_values.count(max_q) > 1:
+            best_moves = [i for i in range(len(remaining_moves)) if q_values[i] == max_q]
+            i = random.choice(best_moves)
         else:
-            min_q = min(self.q_table[state][m] for m in moves)
-            best_moves = [m for m in moves if self.q_table[state][m] == min_q]
-        return random.choice(best_moves)
+            i = q_values.index(max_q)
+        return remaining_moves[i]
+
+    def remaining_moves_from_state(self, state: str) -> list[int]:
+        return [i for i, char in enumerate(state, 1) if char == ' ']
 
     def update_q_table(self, state: str, next_state: str, move: int, reward: float):
-        if state in self.q_table:
-            q_values = self.q_table[state]
-        else:
-            q_values = {a: default_q for a in self.game.get_remaining_moves()}
-            q_values[move] = default_q
-
-        if next_state in self.q_table:
-            max_next_q = max(self.q_table[next_state].values())
-        else:
-            max_next_q = default_q
-
-        if move not in q_values:
-            q_values[move] = default_q
-
-        # reward function
-        # q_values[move] += self.alpha * (reward + self.gamma * max_next_q - q_values[move])
-        q_values[move] = (q_values[move] * (1 - self.alpha)) + (self.alpha * reward) + (self.alpha * self.gamma * max_next_q)
-        self.q_table[state] = q_values
+        next_q_values = [self.get_q_value(next_state, next_move) for next_move in self.remaining_moves_from_state(next_state)]
+        max_next_q = max(next_q_values) if next_q_values else default_q
+        self.q_table[state][move] += self.alpha * (reward + self.gamma * max_next_q - self.q_table[state][move])
 
     def train(self):
-        for episode in trange(self.episodes):
-            original_player: str = self.game.players[0].token
-            current_player = original_player
+
+        for e in trange(self.episodes):
+            state = self.game.get_state()
+            token = self.game.get_tokens()[0]
             while not self.game.game_over():
-                state = self.game.get_state()
-                move = self.choose_move(state, current_player == original_player)
-                self.game.place_token(move, current_player)
+                move = self.choose_move(state)
+                self.game.place_token(move, token)
                 next_state = self.game.get_state()
 
-                if self.game.game_over():
-                    if self.game.check_win(original_player):
-                        reward = 1.0
-                    elif not self.game.get_remaining_moves():
-                        reward = 0.0
-                    else:
-                        reward = -1.0
-                    self.update_q_table(state, next_state, move, reward)
+                if self.game.check_win(token):
+                    reward = 1.0
+                elif self.game.get_remaining_moves() == 0:
+                    reward = 0.0
                 else:
-                    current_player = self.game.get_other(current_player)
-                    # self.game.place_token(random.choice(self.game.get_remaining_moves()), other_player)
-                    # reward = self.game.evaluate_early(current_player, self.game.get_other(current_player))
-                    self.update_q_table(state, next_state, move, 0.0)
-            # self.epsilon -= 1 / self.episodes
-            self.game.reset()
+                    reward = -1.0
+
+                self.update_q_table(state, next_state, move, reward)
+                state = next_state
+                token = self.game.get_other(token)
 
         self.save_q_table(f"q_tables/{self.game.__class__.__name__}.json")
 
